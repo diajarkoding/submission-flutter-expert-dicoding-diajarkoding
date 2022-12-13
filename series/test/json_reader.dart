370cc9d7938a4062f147bb08326d6b2a28d7da93
iskndrm4 <iskndrm4@gmail.com>
2022-12-14 00:39:37 +0700

    json reader

--- series/test/json_reader.dart
+++ series/test/json_reader.dart
@@ -5,5 +5,8 @@ String readJson(String name) {
   if (dir.endsWith('/test')) {
     dir = dir.replaceAll('/test', '');
   }
-  return File('$dir/test/$name').readAsStringSync();
+  if (dir.endsWith('series')) {
+    return File('$dir/test/$name').readAsStringSync();
+  }
+  return File('$dir/series/test/$name').readAsStringSync();
 }
